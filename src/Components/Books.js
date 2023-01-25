de995aa37956d7cec73af119ed54960cfdef061c
unknown <kashif_saleem_23@hotmail.com>
2023-01-25 17:38:52 +0500

    check status functionality

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -6,7 +6,6 @@ import { addedBook, removeBook } from '../redux/books/books';
 export default function Books() {
   const [title, settitle] = useState('');
   const [author, setauthor] = useState('');
-  // const { title, author } = props;
 
   // consuming state
   const books = useSelector((state) => state.books);
@@ -25,24 +24,39 @@ export default function Books() {
   return (
     <>
       <div className="container">
-        <ul className="my-4 border border-3 p-0">
-          <li className="d-flex justify-content-between ms-0 p-3">
+        <div className="my-4 border border-3 p-0">
+          <div>
             {books.map((book) => (
-              <span key={book.id}>
-                <span>{book.title}</span>
-                <span>{book.author}</span>
-                <button className="btn btn-primary mb-3" type="submit" onClick={() => { handleDelete(book.id); }}>Remove</button>
-              </span>
+              <div key={book.id} className="d-flex justify-content-between ms-0 p-3">
+                <span>
+                  Title:
+                  {' '}
+                  {book.title}
+                </span>
+                <span>
+                  Author:
+                  {' '}
+                  {book.author}
+                </span>
+                <button
+                  className="btn btn-primary mb-3"
+                  type="submit"
+                  onClick={() => { handleDelete(book.id); }}
+                >
+                  Remove
+
+                </button>
+              </div>
             ))}
-          </li>
-        </ul>
+          </div>
+        </div>
         <h2 className="fs-2 text">Add New Book</h2>
         <form className="row g-3">
-          <div className="col-8">
-            <input type="text" className="form-control" value={title} onChange={(e) => settitle(e.target.value)} id="inputPassword2" placeholder="Book title" />
+          <div className="col-7">
+            <input type="text" className="form-control" value={title} onChange={(e) => settitle(e.target.value.slice(0, 1).toUpperCase() + e.target.value.slice(1, e.target.value.length).toLocaleLowerCase())} id="inputPassword2" placeholder="Book Title" />
           </div>
-          <div className="col-8">
-            <input type="text" className="form-control" value={author} onChange={(e) => setauthor(e.target.value)} id="inputPassword2" placeholder="Book title" />
+          <div className="col-3">
+            <input type="text" className="form-control" value={author} onChange={(e) => setauthor(e.target.value.slice(0, 1).toUpperCase() + e.target.value.slice(1, e.target.value.length).toLocaleLowerCase())} id="inputPassword2" placeholder="Book Author" />
           </div>
           <div className="col-2">
             <button type="submit" className="btn btn-primary mb-3" onClick={handleSubmit}>Add Book</button>
--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import { React } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 export default function Categories() {
+  // consume the state
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories[0]);
+  const handleLick = () => {
+    dispatch(checkStatus());
+  };
   return (
+    <>
+      {!categories && <button type="submit" onClick={handleLick}>Check Status</button>}
+      <h2>{categories}</h2>
+    </>
 
-    <button type="submit">Check Status</button>
   );
 }
