548c1318d8808e0873aef343bfaa6c3f8d1e12ae
unknown <kashif_saleem_23@hotmail.com>
2023-01-27 19:31:58 +0500

    style card portion for author title and buttons

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -44,45 +44,46 @@ export default function Books() {
         <div className="my-4 p-0 add-gap">
           {
           bookArr.map((e) => (
-            <div key={data.id} className="d-flex flex-column justify-content-between ms-0 p-3 book">
-              <h4 className="book-category">
-                {' '}
-                {e.category}
-              </h4>
-              <h2 className="book-title">
-                {' '}
-                {e.title}
-              </h2>
-              <h6 className="book-author">
-                {' '}
-                {e.author}
-              </h6>
-              <div className="action-buttons">
-                <button
-                  className="button-outline"
-                  type="submit"
-                >
-                  Comments
+            <div key={data.id} className="d-flex justify-content-between ms-0 p-3 book">
+              <div className="">
+                <h4 className="book-category">
+                  {' '}
+                  {e.category}
+                </h4>
+                <h2 className="book-title">
+                  {' '}
+                  {e.title}
+                </h2>
+                <h6 className="book-author">
+                  {' '}
+                  {e.author}
+                </h6>
+                <div className="action-buttons">
+                  <button
+                    className="button-outline"
+                    type="submit"
+                  >
+                    Comments
 
-                </button>
-                <div className="vertical-divider" />
-                <button
-                  className="button-outline"
-                  type="submit"
-                  onClick={() => { handleDelete(e.id); }}
-                >
-                  Remove
+                  </button>
+                  <div className="vertical-divider" />
+                  <button
+                    className="button-outline"
+                    type="submit"
+                    onClick={() => { handleDelete(e.id); }}
+                  >
+                    Remove
 
-                </button>
-                <div className="vertical-divider" />
-                <button
-                  className="button-outline"
-                  type="submit"
-                >
-                  Edit
+                  </button>
+                  <div className="vertical-divider" />
+                  <button
+                    className="button-outline"
+                    type="submit"
+                  >
+                    Edit
 
-                </button>
-                <div className="vertical-divider" />
+                  </button>
+                </div>
               </div>
               <div className="progress-counter">
                 <div />
